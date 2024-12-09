51a8720c5fa8cbb8708de3c7dae0e21311fede64
DorineBattistuta <dorine.battistuta@edu.ece.fr>
2024-12-09 11:42:15 +0100

    V1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import HomePage from './components/HomePage';
 import QuizForm from './components/QuizForm';
 import QuestionSetupPage from './components/QuestionSetupPage';
+import QuizQuestionPage from './components/QuizQuestionPage'; // Importer la page de question
 import './App.css';
 
 function App() {
@@ -20,7 +21,8 @@ function App() {
     const handleStartQuiz = (num) => {
         setNumQuestions(num);
         console.log(`Démarrage du quiz avec ${num} questions !`);
-        // À partir d'ici, vous pouvez rediriger vers une page de quiz
+        // On redirige vers la première question après avoir choisi le nombre de questions
+        setCurrentPage('question1');
     };
 
     const handleGoHome = () => {
@@ -36,6 +38,9 @@ function App() {
             {currentPage === 'questionSetup' && (
                 <QuestionSetupPage onStartQuiz={handleStartQuiz} onGoHome={handleGoHome} />
             )}
+            {currentPage === 'question1' && <QuizQuestionPage questionNumber={1} />}
+            {currentPage === 'question2' && <QuizQuestionPage questionNumber={2} />}
+            {/* Ajoute plus de routes pour les autres questions si nécessaire */}
         </div>
     );
 }
